f88baa88a1f3a1187a2802a64625352ec0e4151a
Porter McGary <56183563+mcgaryp@users.noreply.github.com>
2022-09-22 18:36:06 -0700

    Adjustments that I messed up

--- node-template/create-node-template.sh
+++ node-template/create-node-template.sh
@@ -2,7 +2,7 @@
 
 # GENERAL FUNCTIONS
 function makeFile() {
-    echo $2 > $1
+    echo "$2" > $1
 }
 
 function makeDir() {
@@ -86,165 +86,167 @@ function serverFolder() {
 }
 
 function controllerFolder() {
-    makeDir controllers
-    cd controllers
+    makeDir modules
+    cd modules
 
-    makeFile 'exampleController.js'
+    makeFile 'exampleModule.js'
 
     cd ..
 }
 
 # FILES
 function databaseFile() {
-    makeFile 'database.js' 'const { Client } = require("pg")\n
-    const fs = require("fs")\n
-    const client = new Client()\n
-    \n
-    var db = {\n
-    \tconnect: () => {\n
-    \t\tclient\n
-    \t\t\t.connect()\n
-    \t\t\t.then(() => console.log(`Connected to DB`))\n
-    \t\t\t.catch(err => console.error("connection error", err.stack))\n
-    \t},\n
-    \tdisconnect: () => {\n
-    \t\tclient\n
-    \t\t\t.end()\n
-    \t\t\t.then(() => console.log("Client has disconnected from DB"))\n
-    \t\t\t.end(err => console.error("Error during Cliend Disconnect", err.stack))\n
-    \t},\n
-    \ttest: () => {\n
-    \t\tclient\n
-    \t\t\t.query("SELECT NOW()")\n
-    \t\t\t.then((response) => console.log(response.rows))\n
-    \t\t\t.catch((exception) => console.error(exception))\n
-    \t},\n
-    \tseed: (req, res) => {\n
-    \t\tconst createBuffer = fs.readFileSync(__dirname + "/scripts/create.sql")\n
-    \t\tconst create = createBuffer.toString()\n
-    \t\tclient\n
-    \t\t\t.query(create)\n
-    \t\t\t.then(() => console.log("DB Tables Initialized"))\n
-    \t\t\t.catch(err => console.error("Error Creating Tables", err.stack))\n
-    \n
-    \tconst insertBuffer = fs.readFileSync(__dirname + "./scripts/seed.sql")\n
-    \tconst insert = insertBuffer.toString()\n
-    \t\tclient\n
-    \t\t\t.query(insert)\n
-    \t\t\t.then(() => {\n
-    \t\t\t\tconsole.log("DB Tables Seeded")\n
-    \t\t\t\tres.status(200).send("Success")\n
-    \t\t\t})\n
-    \t\t\t.catch(err => console.error("Error Seeding Tables", err))\n
-    \t}\n
-    }\n'
+    makeFile 'database.js' 'const { Client } = require("pg")
+const fs = require("fs")
+const client = new Client()
+
+var db = {
+    connect: () => {
+        client
+            .connect()
+            .then(() => console.log(`Connected to DB`))
+            .catch(err => console.error("connection error", err.stack))
+    },
+    disconnect: () => {
+        client
+            .end()
+            .then(() => console.log("Client has disconnected from DB"))
+            .end(err => console.error("Error during Cliend Disconnect", err.stack))
+    },
+    test: () => {
+        client
+            .query("SELECT NOW()")
+            .then((response) => console.log(response.rows))
+            .catch((exception) => console.error(exception))
+    },
+    seed: (req, res) => {
+        const createBuffer = fs.readFileSync(__dirname + "/scripts/create.sql")
+        const create = createBuffer.toString()
+        client
+            .query(create)
+            .then(() => console.log("DB Tables Initialized"))
+            .catch(err => console.error("Error Creating Tables", err.stack))
+
+    const insertBuffer = fs.readFileSync(__dirname + "./scripts/seed.sql")
+    const insert = insertBuffer.toString()
+    client
+        .query(insert)
+        .then(() => {
+            console.log("DB Tables Seeded")
+            res.status(200).send("Success")
+        })
+        .catch(err => console.error("Error Seeding Tables", err))
+    }
+}
+
+module.exports = db'
 }
 
 function indexHtmlFile() {
-    makeFile 'index.html' '<!doctype html>\n
-    <html lang="en">\n
-    \t<head>\n
-    \t\t<!-- Required meta tags -->\n
-    \t\t<meta charset="utf-8">\n
-    \t\t<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n
-    \n
-    \t\t<!-- Bootstrap CSS -->\n
-    \t\t<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">\n
-    \n
-    \t\t<title>Hello, the world!</title>\n
-    \t</head>\n
-    \t<body>\n
-    \t\t<div class="jumbotron">\n
-    \t\t\t<div class="container"><h1>Hello World!</h1></div>\n
-    \t</div>\n
-    \n
-    \t\t<!-- Optional JavaScript -->\n
-    \t\t<!-- jQuery first, then Popper.js, then Bootstrap JS -->\n
-    \t\t<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>\n
-    \t\t<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>\n
-    \t\t<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>\n
-    \t</body>\n
-    </html>\n'
+    makeFile 'index.html' '<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <title>Hello, the world!</title>
+    </head>
+    <body>
+        <div class="jumbotron">
+            <div class="container"><h1>Hello World!</h1></div>
+    </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+</html>'
 }
 
 function serverFile() {
-    makeFile 'server.js' 'require("dotenv").config()\n
-    \n
-    const express = require("express")\n
-    const app = express()\n
-    const port = process.env.PORT\n
-    \n
-    // USE\n
-    app.use(express.static("public"))\n
-    \n
-    // POSTS\n
-    \n
-    \n
-    // GETS\n
-    // Send `index.html` file to the browser so the viewer can see the page\n
-    app.get("/", (req, res) => {\n
-        res.sendFile("index.html")\n
-    })\n
-    \n
-    //Server\n
-    app.listen(port, (req, res) => {\n
-        console.log(`Listening on port ${port}`)\n
-    })\n'
+    makeFile 'server.js' 'require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT
+    
+// USE
+app.use(express.static("public"))
+    
+// POSTS
+    
+    
+// GETS
+// Send `index.html` file to the browser so the viewer can see the page
+app.get("/", (req, res) => {
+    res.sendFile("index.html")
+})
+    
+// Server
+app.listen(port, (req, res) => {
+    console.log(`Listening on port ${port}`)
+})'
 }
 
 function envFile() {
-    makeFile '.env' '# For PosgresSQL DB\n
-        USER=""\n
-        PGPASSWORD=""\n
-        PGHOST=""\n
-        PGDATABASE=""\n
-        PGPORT=""\n
-        \n
-        # General\n
-        PORT="4000""
-    '
+    makeFile '.env' '# For PosgresSQL DB
+USER=""
+PGPASSWORD=""
+PGHOST=""
+PGDATABASE=""
+PGPORT=""
+
+# General
+PORT="4000"
+'
 }
 
 function gitIgnoreFile() {
-    makeFile '.gitignore' 'node_modules\n.env'
+    makeFile '.gitignore' 'node_modules
+.env'
 }
 
 function bsConfigFile() {
-    makeFile 'bs-config.js' 'module.exports = {\n
-    \tbrowser: "google chrome",\n
-    \tfiles: ["**/*.css", "**/*.html", "**/*.js"],\n
-    \tignore: ["node_modules"],\n
-    \tnotify: false,\n
-    \tport: 3000,\n
-    \tproxy: "localhost:4000",\n
-    \treloadDelay: 10,\n
-    \tui: false,\n
-    }'
+    makeFile 'bs-config.js' 'module.exports = {
+    browser: "google chrome",
+    files: ["**/*.css", "**/*.html", "**/*.js"],
+    ignore: ["node_modules"],
+    notify: false,
+    port: 3000,
+    proxy: "localhost:4000",
+    reloadDelay: 10,
+    ui: false,
+}'
 }
 
 function packageFile() {
-    makeFile 'package.json' '{\n
-    \t"name": "",\n
-    \t"author": {\n
-    \t\t"name": ""\n
-    \t},\n
-    \t"version": "0.0.1",\n
-    \t"scripts": {\n
-    \t\t"dev": "nodemon server/server.js",\n
-    \t\t"ui": "browser-sync start --config bs-config.js"\n
-    \t},\n
-    \t"repository": {\n
-    \t\t"type": "git",\n
-    \t\t"url": "https://github.com/"\n
-    \t},\n
-    \t"dependencies": {\n
-    \t\t"nodemon": "^2.0.20",\n
-    \t\t"express": "^4.18.1",\n
-    \t\t"pg": "8.8.0",\n
-    \t\t"dotenv": "16.0.2",\n
-    \t\t"browser-sync": "2.27.10"\n
-    \t}\n
-    }'
+    makeFile 'package.json' '{
+    "name": "",
+    "author": {
+        "name": ""
+    },
+    "version": "0.0.1",
+    "scripts": {
+        "dev": "nodemon server/server.js",
+        "ui": "browser-sync start --config bs-config.js"
+    },
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/"
+    },
+    "dependencies": {
+        "nodemon": "^2.0.20",
+        "express": "^4.18.1",
+        "pg": "8.8.0",
+        "dotenv": "16.0.2",
+        "browser-sync": "2.27.10"
+    }
+}'
 }
 
 function createProject() {
@@ -264,16 +266,17 @@ function main() {
         exit
     fi
 
-    # createProject $projectName
+    createProject $projectName
+    # databaseFile
 
-    if [ $2 == "--config-git" ]
+    if [ "$2" == "--config-git" ]
     then
         # Create git repo
         git init
     fi
 
     # Run npm install
-    npm install
+    # npm install
 }
 
 main $1 $2
